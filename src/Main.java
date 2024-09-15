490fd217ba4b4fb4e95c611a3e5cb20c2911f3b2
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-15 20:15:22 +0200

    Instantiate an instance of Rectangle class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        Rectangle rectangle;
+        double width, height, x, y;
+
+        System.out.println("Enter the width of the rectangle: ");
+        width = input.nextDouble();
+        input.nextLine();
+        System.out.println("Enter the height of the rectangle: ");
+        height = input.nextDouble();
+        input.nextLine();
+        System.out.println("Enter the x of the rectangle: ");
+        x = input.nextDouble();
+        input.nextLine();
+        System.out.println("Enter the y of the rectangle: ");
+        y = input.nextDouble();
+        rectangle = new Rectangle(x, y, width, height);
+
+        System.out.printf("The area and perimeter of the rectangle are: %.2f and %.2f\n",
+                rectangle.calculateArea(), rectangle.calculatePerimeter());
+
+        rectangle.setXY(5, 5);
+        rectangle.setHeight(5);
+        rectangle.setWidth(5);
+
+        System.out.printf("The area and perimeter of the rectangle are: %.2f and %.2f",
+                rectangle.calculateArea(), rectangle.calculatePerimeter());
     }
 }
