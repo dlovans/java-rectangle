763dab5de9df0f42bce43871cfbbbda120c671e9
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-15 19:34:21 +0200

    Declare Rectangle fields

---
+++ src/Rectangle.java
@@ -0,0 +1,21 @@
+public class Rectangle {
+    /**
+     * x coordinate.
+     */
+    private double x;
+
+    /**
+     * y coordinate.
+     */
+    private double y;
+
+    /**
+     * Width of rectangle.
+     */
+    private double width;
+
+    /**
+     * Height of rectangle.
+     */
+    private double height;
+}
