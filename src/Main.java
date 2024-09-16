bf77baa421c5de3dbe85bdbde7ce678cd6e82222
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-16 17:29:57 +0200

    Refactor main method

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Rectangle rectangle;
+        Rectangle rectangleOne;
+        Rectangle rectangleTwo;
         double width, height, x, y;
 
-        System.out.println("Enter the width of the rectangle: ");
+        System.out.println("First rectangle: ");
+        System.out.println("Width: ");
         width = input.nextDouble();
         input.nextLine();
-        System.out.println("Enter the height of the rectangle: ");
+        System.out.println("Height: ");
         height = input.nextDouble();
         input.nextLine();
-        System.out.println("Enter the x of the rectangle: ");
+        System.out.println("X-coordinate: ");
         x = input.nextDouble();
         input.nextLine();
-        System.out.println("Enter the y of the rectangle: ");
+        System.out.println("Y-coordinate: ");
         y = input.nextDouble();
-        rectangle = new Rectangle(x, y, width, height);
+        input.nextLine();
+        rectangleOne = new Rectangle(x, y, width, height);
 
-        System.out.printf("The area and perimeter of the rectangle are: %.2f and %.2f\n",
-                rectangle.calculateArea(), rectangle.calculatePerimeter());
+        System.out.println("Second rectangle: ");
+        System.out.println("Width: ");
+        width = input.nextDouble();
+        input.nextLine();
+        System.out.println("Height: ");
+        height = input.nextDouble();
+        input.nextLine();
+        System.out.println("X-coordinate: ");
+        x = input.nextDouble();
+        input.nextLine();
+        System.out.println("Y-coordinate: ");
+        y = input.nextDouble();
+        rectangleTwo = new Rectangle(x, y, width, height);
 
-        rectangle.setXY(5, 5);
-        rectangle.setHeight(5);
-        rectangle.setWidth(5);
+        System.out.printf("First rectangle area and perimenter: %.2f and %.2f\n",
+                rectangleOne.calculateArea(), rectangleOne.calculatePerimeter());
 
-        System.out.printf("The area and perimeter of the rectangle are: %.2f and %.2f",
-                rectangle.calculateArea(), rectangle.calculatePerimeter());
+        System.out.printf("Second rectangle area and perimenter: %.2f and %.2f\n",
+                rectangleTwo.calculateArea(), rectangleTwo.calculatePerimeter());
+
+        Rectangle.checkCollision(rectangleOne, rectangleTwo);
     }
 }
